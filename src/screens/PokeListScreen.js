2f5069aebbdc3c3572cd680d8cb61e5b12919470
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2020-10-24 20:50:12 -0600

    Creacion de la carpeta donde se encotraran los  recursos de la aplicacion (pantallas y api)

---
+++ src/screens/PokeListScreen.js
@@ -0,0 +1,14 @@
+//Importar las librerias necesarias
+import React from "react";
+import {StyleSheet, Text} from "react-native";
+
+//variable que contiene la pantalla(renderizar)
+const PokeListScreen = () => {
+    return <Text>Lista de pokemones</Text>;
+
+}
+
+const styles = StyleSheet.create({});
+
+export default PokeListScreen;
+
