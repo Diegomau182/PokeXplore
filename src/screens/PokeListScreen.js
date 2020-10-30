e197f5c8b6db94aa8dff4eb3d1c9b7a8443ec5a9
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2020-10-30 15:25:56 -0600

    Diseño de la pantalla PokeListScreen terminado

--- src/screens/PokeListScreen.js
+++ src/screens/PokeListScreen.js
@@ -1,9 +1,8 @@
 //Importar las librerias necesarias
 //import { Col } from "native-base";
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import fonts from "expo-font";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { ImageBackground, StyleSheet, Text, View} from "react-native";
+import {Input, Button} from "native-base";
 const fondo = {uri: "https://media.discordapp.net/attachments/684522611488849983/770751505724080138/fondo.png"}
 //variable que contiene la pantalla(renderizar)
 const PokeListScreen = () => (
@@ -11,7 +10,7 @@ const PokeListScreen = () => (
       <ImageBackground source={fondo} style={styles.image}>
         <View style={styles.containerDos}>
         <View style={{left:20, top:7, width: 250, height: 30, backgroundColor:"#fc0000"}}>
-          <Text style={{color:"#ffffff", textAlign:"center", fontSize:25}}>POKEXPLORE</Text>
+          <Text style={{color:"#ffffff", textAlign:"center", fontSize:25}}>POKEXPLORER</Text>
         </View>
           <View style={styles.marco}>
             <View style={{right:230, top:-23, width: 275, height: 175, backgroundColor:"#e8cc57"}}>
@@ -22,8 +21,17 @@ const PokeListScreen = () => (
               </View>  
             </View>
           </View>
+          <View style={{width:200,right:230,top:283,height:30}}>
+          <Input placeholder="Search" backgroundColor="#ffffff"></Input>     
+          </View> 
+            <Button style={{justifyContent:"center",width:100,height:30, right:225, top:283, backgroundColor:"#fc0000" }}>
+              <Text style={{color:"#ffffff"}}>Search</Text>
+            </Button>
+            <Button style={{justifyContent:"center",width:200,height:50, right:200, top:300, backgroundColor:"#fc0000" }}>
+              <Text style={{color:"#ffffff"}}>More Info</Text>
+            </Button>
           <View style={styles.marcoPequeños}>
-            <View style={{left:-100 ,top:-40 ,width: 300, height: 271, backgroundColor:"#fc0000"}}>
+            <View style={{left:-400 ,top:-40 ,width: 290, height: 281, backgroundColor:"#fc0000"}}>
               
             </View>
           </View>
