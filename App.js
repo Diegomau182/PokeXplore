b7c14c1d6fe54c0b37b786af14dcf1bbde5fbdf5
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2020-11-10 09:06:03 -0600

    se implemento la inteligencia navigation

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import PokeListScreen from "./src/screens/PokeListScreen"
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import PokeListScreen from "./src/screens/PokeListScreen";
+import PokeSearchResults from "./src/screens/PokeSearchResults";
+
+//creacion de nuestra navegacion basadas en stack (pilas)
+const Stack = createStackNavigator(); 
 
 export default function App() {
-  return <PokeListScreen></PokeListScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Lista de Pokemos">
+        <Stack.Screen name="Lista de Pokemos" component={PokeListScreen}/>
+        <Stack.Screen name="Resultado Busqueda" component={PokeSearchResults}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
