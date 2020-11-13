22f1902cecab1589203ddd64ba23a5560102019f
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2020-11-13 11:36:33 -0600

    quita el titulo de ListPokeScreen de la pantalla

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Lista de Pokemos">
-        <Stack.Screen name="Lista de Pokemos" component={PokeListScreen}/>
+        <Stack.Screen name="Lista de Pokemos" component={PokeListScreen} options={{headerShown:false}}/>
         <Stack.Screen name="Resultado Busqueda" component={PokeSearchResults}/>
       </Stack.Navigator>
     </NavigationContainer>
