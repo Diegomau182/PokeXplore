3ea23a5099346f962628bbc8c9c059576f2d9a69
Carlos <c.amarquez14@hotmail.com>
2020-11-13 20:33:17 -0600

    x

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import PokeListScreen from "./src/screens/PokeListScreen"
+//import PokeListScreen from "./src/screens/PokeListScreen"
+import PokeDataScreen from "./src/screens/PokeDataScreen"
 
 export default function App() {
-  return <PokeListScreen></PokeListScreen>;
+  //return <PokeListScreen></PokeListScreen>;
+  return <PokeDataScreen></PokeDataScreen>;
 }
 
